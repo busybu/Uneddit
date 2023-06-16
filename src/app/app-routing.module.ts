3911f27b89ab36f6d5ca918c78a32b71059eae28
busybu <gabrielle.fialkoski@hotmail.com>
2023-06-16 11:43:19 -0300

    add: components. error: sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedPageComponent } from './feed-page/feed-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FeedPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeedPageComponent } from './feed-page/feed-page.component';
+import { PostUserComponent } from './post-user/post-user.component';
+import { MatButtonModule } from '@angular/material/button';
+import { SideBarComponent } from './side-bar/side-bar.component';
+
 
 @NgModule({
     declarations: [
         AppComponent,
+        PostUserComponent,
+        FeedPageComponent,
+        SideBarComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
@@ -20,7 +28,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         MatToolbarModule,
         MatIconModule,
         BrowserAnimationsModule,
-        NavComponent
+        NavComponent,
+        MatButtonModule,
     ]
 })
 export class AppModule { }
